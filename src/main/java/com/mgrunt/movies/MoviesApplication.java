849b5a12e075ec0bd680b4aea4bbee7540a4b648
Mateusz Gruntkowski <mati.gruntkowski@gmail.com>
2025-08-11 12:12:12 +0200

    test commit

--- src/main/java/com/mgrunt/movies/MoviesApplication.java
+++ src/main/java/com/mgrunt/movies/MoviesApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class MoviesApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Starting Movies Application...");
 		SpringApplication.run(MoviesApplication.class, args);
 	}
 
