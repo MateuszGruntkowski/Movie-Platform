64dda6ef1bc7fd4f1987ee4d9de198da2d4b6f2f
Mateusz Gruntkowski <mati.gruntkowski@gmail.com>
2026-07-12 18:22:11 +0200

    Rename MovieDataDto to MovieSeedDto, align trailerUrl field name in seed JSON

--- src/main/java/com/mgrunt/movies/domain/dtos/MovieSeedDto.java
+++ src/main/java/com/mgrunt/movies/domain/dtos/MovieSeedDto.java
@@ -9,11 +9,16 @@ import lombok.NoArgsConstructor;
 import java.time.LocalDate;
 import java.util.List;
 
+/**
+ * The data structure of the seed file (src/main/resources/data/movies.json),
+ * used exclusively by MovieDataLoader to pre-populate the database.
+ * Not to be confused with API response DTOs (MovieDetailsResponse, MovieSearchResponse).
+ */
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
-public class MovieDataDto {
+public class MovieSeedDto {
     private String imdbId;
     private Long tmdbId;
     private String title;
